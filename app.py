26d8582d43f08fed1230bc02fb553bf9a6ec1f49
raja <raja.s.dasgupta@gmail.com>
2022-08-03 10:46:31 +0530

    added exception handler and logger

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask
 import sys
-#from forest.logger import logging
-#from forest.exception import ForestException
+from forest.logger import logging
+from forest.exception import ForestException
 
 app=Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def home():
     try:
-        raise Exception("we are testing custom expection")
+        raise ForestException("we are testing custom expection")
     except Exception as e:
-        forest = Exception(e,sys)
-        #logging.info(forest.error_message)    
-        #logging.info("We are testing the logging module")
-    return " Testing exception in Forest project. Added CI/CD pipleine as well"
+        forest = ForestException(e,sys)
+        logging.info(forest.error_message)    
+        logging.info("We are testing the logging module")
+    return " Testing exception in Forest project. Added exception handling and logging"
 
 if __name__=='__main__':
     app.run(debug=True)
